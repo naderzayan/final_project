564fcf6b3d9a62b877eb9f0c57c2ac2c1bb8081b
aseelgamal22 <aseelbadr501@gmail.com>
2025-02-25 01:05:04 +0200

    Create CounterContextt.jsx

---
+++ final project/src/components/CounterContextt.jsx
@@ -0,0 +1,41 @@
+import { createContext,useState } from 'react'
+
+
+  const CounterContext= createContext();
+
+
+
+   function CounterProvider ({children})
+
+
+  {
+    const [myCounter, setmyCounter] = useState(0);
+  
+return(
+
+<CounterContext.Provider value={{myCounter,setmyCounter}   } > 
+  {children}
+
+</CounterContext.Provider>
+)
+
+
+  }
+  export{CounterProvider,CounterContext}
+
+
+
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
