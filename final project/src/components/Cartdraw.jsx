047fed15d9b86f99035be6496b643ed544d5b978
aseelgamal22 <aseelbadr501@gmail.com>
2025-02-25 01:06:04 +0200

    Create Cartdraw.jsx

---
+++ final project/src/components/Cartdraw.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export default function Cartdraw({image,title,quan,price}) {
+
+
+
+
+
+
+
+  return (
+    <div>
+      drawwwwww
+
+<img src={ ` ${image} ` } alt="" />
+{title}
+{price}
+{quan}
+
+
+
+
+
+
+        
+    </div>
+  )
+}
