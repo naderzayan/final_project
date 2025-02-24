8c46af28720ec24e470cec4e7f74c4db26b2bb23
aseelgamal22 <aseelbadr501@gmail.com>
2025-02-25 00:48:30 +0200

    Update Data.jsx
    this is the new data

--- final project/src/data/Data.jsx
+++ final project/src/data/Data.jsx
@@ -1,40 +1,61 @@
-const postsData = [
-    {
-        id: 1,
-        image: "watch1.jpg",
-        title: "Drone",
-        Category: "Drones Helicopter",
-        beforPrice: "$720.00",
-        afterPrice: "$540.00",
-        tags: ["Watch"],
-    },
-    {
-        id: 2,
-        image: "watch2.jpg",
-        title: "Watch",
-        Category: "Women Gold Watch",
-        beforPrice: "$640.00",
-        afterPrice: "$540.00",
-        tags: ["Watch"],
-    },
-    {
-        id: 3,
-        image: "watch3.jpg",
-        title: "Watch",
-        Category: "Diamond Watches",
-        beforPrice: "$310.00",
-        afterPrice: "$210.00",
-        tags: ["Watch"],
-    },
-    {
-        id: 4,
-        image: "watch4.jpg",
-        title: "HD LED TV",
-        Category: "LED Television",
-        beforPrice: "$310.00",
-        afterPrice: "$210.00",
-        tags: ["Watch"],
-    },
-];
+const shopdata=[
+{ id:1, title:'3d Glass', amount:"yello"},
+{ id:2, title:"Bluetooth" ,amount: "(1)"},
+  { id:3, title:"Camera", amount: "(6)"},
+    {  id:4 , title:"Drone" , amount:"(2)"},
+    
+    {  id:5 , title:"future product" , amount:"(1)"},
+    {  id:6 , title:"Gamepad" , amount:"(1)"},
+    {  id:7 , title:"google glass" , amount:"(1)"},
+    {  id:8 , title:"headphone" , amount:"(2)"},
+    {  id:9 , title:"laptop" , amount:"(3)"},
+    {  id:10 , title:"LED tv" , amount:"(3)"},
+    {  id:11, title:"light" , amount:"(1)"},
+    {  id:12 , title:"mobile" , amount:"(1)"},
+    {  id:13 , title:"portable" , amount:"(2)"},
+    {  id:1 , title:"projector" , amount:"(1)"},
+    {  id:1 , title:"Samsung" , amount:"(2)"},
+   
+    
+]
+export{shopdata};
 
-export { postsData };
+const watchdata=[
+    { id:0, category:"test" , title:" 3D Glass", image:"/src/watch1.jpg " ,price:"$540.00" ,image2:"/src/watch7.jpg" ,quantity:0 },
+
+
+
+    { id:1, category:"test1" , title:"3D VR Glass", image:"/src/watch2.jpg " ,price:"$245.00" ,image2:"/src/watch1.jpg" ,quantity:0 },
+
+
+
+    { id:2, category:"test2" , title:"bluetooth speaker", image:"/src/watch3.jpg " , price:"$70.00" ,image2:"/src/watch2.jpg" ,quantity:0  },
+    { id:3, category:"test3" , title:"colorful led lights", image:"/src/watch4.jpg " ,price:"$90.00",image2:"/src/watch3.jpg" ,quantity:0   },
+
+
+
+    { id:3, category:"test4" , title:"Diamon Wtches", image:"/src/watch5.jpg "  ,price:"$210.00",image2:"/src/watch4.jpg",quantity:0   },
+
+
+
+    { id:5, category:"test5" , title:"Drone wifi FPV", image:"/src/watch6.jpg ", price:"$250.00",image2:"/src/watch5.jpg"  ,quantity:0  },
+
+
+
+    { id:6, category:"test6" , title:"Drone helicopter", image:"/src/watch7.jpg " ,price:"$54.00" ,image2:"/src/watch6.jpg",quantity:0   },
+
+
+
+    { id:7, category:"test7" , title:"Fuji film Camera", image:"/src/watch8.jpg " ,price:"$200.00",image2:"/src/watch7.jpg",quantity:0    },
+
+
+
+    
+
+
+
+
+
+
+]
+export {watchdata};
