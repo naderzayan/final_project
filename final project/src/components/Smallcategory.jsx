a186247ed3eefee152ae13b1b6e00825aea334ec
aseelgamal22 <aseelbadr501@gmail.com>
2025-02-25 00:59:55 +0200

    Create Smallcategory.jsx

---
+++ final project/src/components/Smallcategory.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Smallcatergory({ title, amount }) {
+    return (
+        <div>
+            <div >
+                
+        
+                {title}{amount}
+
+
+            </div>
+
+        </div>
+    )
+}
